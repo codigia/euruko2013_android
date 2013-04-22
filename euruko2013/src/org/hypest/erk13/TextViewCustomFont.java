dd9ec2d19da6251c2f8edd8db254b66725f0fd15
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2013-04-22 15:51:06 +0300

    Using a glyph font for the menu icons

---
+++ euruko2013/src/org/hypest/erk13/TextViewCustomFont.java
@@ -0,0 +1,48 @@
+package org.hypest.erk13;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.TextView;
+
+public class TextViewCustomFont extends TextView {
+	private static final String TAG = "TextView";
+
+	public TextViewCustomFont(Context context) {
+		super(context);
+	}
+
+	public TextViewCustomFont(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		setCustomFont(context, attrs);
+	}
+
+	public TextViewCustomFont(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		setCustomFont(context, attrs);
+	}
+
+	private void setCustomFont(Context ctx, AttributeSet attrs) {
+		TypedArray a = ctx.obtainStyledAttributes(attrs,
+				R.styleable.TextViewCustomFont);
+		String customFont = a.getString(R.styleable.TextViewCustomFont_customFont);
+		setCustomFont(ctx, customFont);
+		a.recycle();
+	}
+
+	public boolean setCustomFont(Context ctx, String asset) {
+		Typeface tf = null;
+		try {
+			tf = Typeface.createFromAsset(ctx.getAssets(), asset);
+		} catch (Exception e) {
+			Log.e(TAG, "Could not get typeface: " + e.getMessage());
+			return false;
+		}
+
+		setTypeface(tf);
+		return true;
+	}
+
+}
